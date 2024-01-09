1516aaf4d5593c6049c95b5aa85561349769525b
EmilioAG99 <luis.emilio99@hotmail.com>
2024-01-09 15:55:33 -0600

    user logic added

---
+++ internal/auth/auth.go
@@ -0,0 +1,28 @@
+package auth
+
+import (
+	"errors"
+	"net/http"
+	"strings"
+)
+
+// extracts an API key from the headers of an HHTP request
+// Example:
+// Authorization: Apikey {insert api key here}
+func GetAPIKey(headers http.Header) (string, error) {
+	val := headers.Get("Authorization")
+	if val == "" {
+		return "", errors.New("no authentication info")
+	}
+
+	vals := strings.Split(val, " ")
+	if len(vals) != 2 {
+		return "", errors.New("no authentication info found")
+	}
+	if vals[0] != "ApiKey" {
+		return "", errors.New("malformed first parth of auth header")
+	}
+
+	return vals[1], nil
+
+}
--- main.go
+++ main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/emilioag99/rssagg/internal/database"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
+
+type apiConfig struct {
+	DB *database.Queries
+}
 
 func main() {
 	godotenv.Load(".env")
@@ -17,11 +24,27 @@ func main() {
 	if portString == "" {
 		log.Fatal("Port is not found in the environment")
 	}
-	router := chi.NewRouter()
 
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		log.Fatal("DB_url not found in the environment")
+	}
+
+	conn, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal("Cant connect to database")
+	}
+
+	queries := database.New(conn)
+
+	apiCfg := apiConfig{
+		DB: queries,
+	}
+
+	router := chi.NewRouter()
 	router.Use(cors.Handler(cors.Options{
 		AllowedOrigins:   []string{"https://*", "http://*"},
-		AllowedMethods:   []string{"GET	", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"*"},
 		ExposedHeaders:   []string{"Link"},
 		AllowCredentials: false,
@@ -31,6 +54,8 @@ func main() {
 	v1Router := chi.NewRouter()
 	v1Router.Get("/healthz", handleReadiness)
 	v1Router.Get("/error", handleErr)
+	v1Router.Post("/users", apiCfg.handlerCreateUser)
+	v1Router.Get("/users", apiCfg.handleGetUserByAPIKey)
 
 	router.Mount("/v1", v1Router)
 
@@ -40,7 +65,7 @@ func main() {
 	}
 
 	log.Printf("Server starting on port %v", portString)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
 	}
